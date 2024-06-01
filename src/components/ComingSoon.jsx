d71948aae78827674a38591811cba9de53752e6f
Igor Sinko <igor@DESKTOP-6F7DV4M>
2024-06-02 01:26:20 +0200

    Initialize gh-pages branch

---
+++ src/components/ComingSoon.jsx
@@ -0,0 +1,14 @@
+// src/ComingSoon.js
+
+import React from 'react';
+import logo from '../assets/logo.jpg'; // Import the image
+
+function ComingSoon() {
+    return (
+        <div className="coming-soon">
+            <h1>Coming Soon!</h1>
+        </div>
+    );
+}
+
+export default ComingSoon;
